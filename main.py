50a5b92596c9eff9f1aff5d6fdbb462f4dd02b0f
Mete013 <71598037+Mete013@users.noreply.github.com>
2021-05-26 13:50:24 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+#daxil elədik
+
 import telebot
 import time
-from googletrans import Translator
+from googletrans import Translator 
+
+#tokeni falan gösdərdiy
 
 TOKEN = "1801568916:AAEToeCtmwjKkuU2mv9Eb9ff0xF9w4vUUvI"
 bot = telebot.TeleBot(TOKEN)
 translator = Translator()
+
+#bu nədi bilmirəm aq
 
 class Bthelper:
     def __init__(self):
@@ -19,13 +25,15 @@ class Bthelper:
         return self.dest, self.src
 bt_help = Bthelper()
 
+#start basanda bunu deyəcəy
+
 @bot.message_handler(commands=["start"])
 
 def start(message):
       bot.send_message(message.chat.id, "Salam👋 Xoşgəldiniz :) \n Botdan istifadə etmək üçün '/' işarəsinə klikləyə bilərsiz. \n Azərbaycan dilindən İngilis dilinə tərcümə etmək istıyirsizsə '/en' seçiminə, \n İstənilən dildən Azərbaycan dilinə tərcümə etmək istəsəz isə '/tercume' seçiminə tıklayın.")
 
 
-
+#burda gic-gic sözlərə cavab verir sj
 
 @bot.message_handler(content_types=['text'])    
 def send_text(message):
@@ -40,6 +48,7 @@ def send_text(message):
  elif message.text.lower() == 'necəsən?':
   bot.send_message(message.chat.id, 'Təki sən yaxşı ol canım ay ' + message.chat.first_name + '!')
 
+#bu da nədi bilmirəm aq
 
 while True:
     try:
