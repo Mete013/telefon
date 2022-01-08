3251879cca17e8415092fb1e5153cff8be5abfcd
Mete013 <71598037+Mete013@users.noreply.github.com>
2022-01-08 14:30:13 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,55 @@
 
 import telebot
 import time
+import logging
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
 from googletrans import Translator 
+translator = Translator()
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+def to_az(bot, update):
+    content = update.message.text[3:] # `/az `
+    translate = translator.translate(content, dest='az') 
+    bot.send_message(chat_id=update.message.chat_id, text=f'{content} = {translate.text}')
+
+def to_eng(bot, update):
+    content = update.message.text[3:] # `/eng `
+    translate = translator.translate(content, dest='en')
+    bot.send_message(chat_id=update.message.chat_id, text=f'{content} = {translate.text}')    
+    
+def error(bot, update):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', bot, update.error)
+    
+
+def main():
+    updater = Updater("1801568916:AAEToeCtmwjKkuU2mv9Eb9ff0xF9w4vUUvI")
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("az", to_az))
+    dp.add_handler(CommandHandler("en", to_eng))
+    
+
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()    
+    
 
 #tokeni falan gösdərdiy
 
